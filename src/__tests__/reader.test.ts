bf90311cc746ca352e53f15eb8df6dcbff9fc457
Janez Pintar <pintarj.zero@gmail.com>
2019-03-23 19:28:11 +0100

    string reader created

---
+++ src/__tests__/reader.test.ts
@@ -0,0 +1,16 @@
+import {Reader, StringReader} from '../reader';
+
+test('string-reader', () => {
+    var reader: Reader = new StringReader('pikachu');
+    expect(reader.read()).toBe('p');
+    expect(reader.read()).toBe('i');
+    expect(reader.read()).toBe('k');
+    expect(reader.read()).toBe('a');
+    expect(reader.read()).toBe('c');
+    expect(reader.read()).toBe('h');
+    expect(reader.read()).toBe('u');
+    expect(reader.read()).toBe('');
+    expect(reader.read()).toBe('');
+    expect(reader.read()).toBe('');
+});
+
---
+++ src/reader.ts
@@ -0,0 +1,22 @@
+
+export interface Reader {
+    read(): string;
+}
+
+export class StringReader implements Reader {
+    private index: number;
+
+    constructor(private source: string) {
+        this.index = 0;
+    }
+
+    public read(): string {
+        if (this.index == this.source.length)
+            return '';
+
+        var value = this.source[this.index];
+        this.index += 1;
+        return value;
+    }
+}
+
