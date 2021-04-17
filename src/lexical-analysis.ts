7f1051fd2570f85516b411b8b3453723fd587902
Janez Pintar <pintarj.zero@gmail.com>
2021-04-17 23:37:44 +0200

    basic .erz lexical analysis implemented #10

---
+++ src/lexical-analysis.ts
@@ -0,0 +1,98 @@
+import { SourceReader } from './source/source-reader';
+import { Locatable, Location, Point } from './source/location';
+import { CodeError } from './error';
+
+export enum SymbolType {
+    IDENTIFIER,
+    VARIABLE,
+    PRODUCTION,
+    EPSILON,
+    BRACES_LEFT,
+    BRACES_RIGHT,
+    EOF
+}
+
+export interface Symbol {
+    type: SymbolType;
+    location: Locatable;
+    lexeme: string;
+}
+
+const REGEX_ID_START = /^\p{L}$/u;
+const REGEX_ID_CONTINUE = /^[\p{L}\d_]$/u;
+
+export function parse(reader: SourceReader): Symbol[] {
+    const symbols: Symbol[] = [];
+
+    while (true) {
+        let lexeme = reader.read();
+        let location: Locatable = reader.get_point();
+        let type: SymbolType|undefined;
+
+        if (lexeme === '')
+            break;
+
+        if (/^\s$/.test(lexeme)) {
+            continue;
+        } else if (lexeme === '{') {
+            type = SymbolType.BRACES_LEFT;
+        } else if (lexeme === '}') {
+            type = SymbolType.BRACES_RIGHT;
+        } else if (REGEX_ID_START.test(lexeme)) {
+            let last_point: Point|undefined;
+
+            while (true) {
+                const c = reader.read();
+
+                if (!REGEX_ID_CONTINUE.test(c)) {
+                    reader.revoke();
+                    break;
+                } else {
+                    lexeme += c;
+                    last_point = reader.get_point();
+                }
+            }
+
+            if (last_point !== undefined)
+            location = new Location(location as Point, last_point);
+
+            switch (lexeme) {
+                case 'variable': {
+                    type = SymbolType.VARIABLE;
+                    break;
+                }
+
+                case 'production': {
+                    type = SymbolType.PRODUCTION;
+                    break;
+                }
+
+                case 'epsilon': {
+                    type = SymbolType.EPSILON;
+                    break;
+                }
+
+                default: {
+                    type = SymbolType.IDENTIFIER;
+                    break;
+                }
+            }
+        } else {
+            throw new CodeError(reader.file, location, `Unknown lexeme \`${lexeme}\``);
+        }
+
+        symbols.push({
+            type,
+            location,
+            lexeme
+        });
+    }
+
+    symbols.push({
+        type: SymbolType.EOF,
+        location: reader.get_point(),
+        lexeme: ''
+    });
+
+    return symbols;
+}
---
+++ tests/lexical-analysis.test.ts
@@ -0,0 +1,146 @@
+import { SymbolType, parse } from '@dist/lexical-analysis';
+import { SourceReader } from '@dist/source/source-reader';
+import { StringReader } from '@dist/reader';
+import { Location, Point } from '@dist/source/location';
+
+test('parse-empty', () => {
+    const source = ``;
+    const result = parse(new SourceReader(new StringReader(source)));
+    expect(result).toStrictEqual([{
+        type: SymbolType.EOF,
+        location: new Point(1, 1),
+        lexeme: ''
+    }]);
+});
+
+test('parse-newline', () => {
+    const source = `\n`;
+    const result = parse(new SourceReader(new StringReader(source)));
+    expect(result).toStrictEqual([{
+        type: SymbolType.EOF,
+        location: new Point(2, 1),
+        lexeme: ''
+    }]);
+});
+
+describe('parse-identifier', () => {
+    test('single-char', () => {
+        const source = `x`;
+        const result = parse(new SourceReader(new StringReader(source)));
+        expect(result).toStrictEqual([{
+            type: SymbolType.IDENTIFIER,
+            location: new Point(1, 1),
+            lexeme: 'x'
+        }, {
+            type: SymbolType.EOF,
+            location: new Point(1, 2),
+            lexeme: ''
+        }]);
+    });
+
+    test('multi-char', () => {
+        const source = `gamma`;
+        const result = parse(new SourceReader(new StringReader(source)));
+        expect(result).toStrictEqual([{
+            type: SymbolType.IDENTIFIER,
+            location: new Location(new Point(1, 1), new Point(1, 5)),
+            lexeme: 'gamma'
+        }, {
+            type: SymbolType.EOF,
+            location: new Point(1, 6),
+            lexeme: ''
+        }]);
+    });
+});
+
+test('parse-variable', () => {
+    const source = `variable`;
+    const result = parse(new SourceReader(new StringReader(source)));
+    expect(result).toStrictEqual([{
+        type: SymbolType.VARIABLE,
+        location: new Location(new Point(1, 1), new Point(1, 8)),
+        lexeme: 'variable'
+    }, {
+        type: SymbolType.EOF,
+        location: new Point(1, 9),
+        lexeme: ''
+    }]);
+});
+
+test('parse-production', () => {
+    const source = `production`;
+    const result = parse(new SourceReader(new StringReader(source)));
+    expect(result).toStrictEqual([{
+        type: SymbolType.PRODUCTION,
+        location: new Location(new Point(1, 1), new Point(1, 10)),
+        lexeme: 'production'
+    }, {
+        type: SymbolType.EOF,
+        location: new Point(1, 11),
+        lexeme: ''
+    }]);
+});
+
+test('parse-epsilon', () => {
+    const source = `epsilon`;
+    const result = parse(new SourceReader(new StringReader(source)));
+    expect(result).toStrictEqual([{
+        type: SymbolType.EPSILON,
+        location: new Location(new Point(1, 1), new Point(1, 7)),
+        lexeme: 'epsilon'
+    }, {
+        type: SymbolType.EOF,
+        location: new Point(1, 8),
+        lexeme: ''
+    }]);
+});
+
+test('parse-brace-left', () => {
+    const source = `{`;
+    const result = parse(new SourceReader(new StringReader(source)));
+    expect(result).toStrictEqual([{
+        type: SymbolType.BRACES_LEFT,
+        location: new Point(1, 1),
+        lexeme: '{'
+    }, {
+        type: SymbolType.EOF,
+        location: new Point(1, 2),
+        lexeme: ''
+    }]);
+});
+
+test('parse-brace-right', () => {
+    const source = `}`;
+    const result = parse(new SourceReader(new StringReader(source)));
+    expect(result).toStrictEqual([{
+        type: SymbolType.BRACES_RIGHT,
+        location: new Point(1, 1),
+        lexeme: '}'
+    }, {
+        type: SymbolType.EOF,
+        location: new Point(1, 2),
+        lexeme: ''
+    }]);
+});
+
+test('skip-whitespaces', () => {
+    const source = ` { } \n } \n\n `;
+    const result = parse(new SourceReader(new StringReader(source)));
+    expect(result).toStrictEqual([{
+        type: SymbolType.BRACES_LEFT,
+        location: new Point(1, 2),
+        lexeme: '{'
+    }, {
+        type: SymbolType.BRACES_RIGHT,
+        location: new Point(1, 4),
+        lexeme: '}'
+    }, {
+        type: SymbolType.BRACES_RIGHT,
+        location: new Point(2, 2),
+        lexeme: '}'
+    }, {
+        type: SymbolType.EOF,
+        location: new Point(4, 2),
+        lexeme: ''
+    }]);
+});
