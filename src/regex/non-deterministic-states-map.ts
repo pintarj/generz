ac1145dbab768d66e4e62561efcb4c9e575f19ff
Janez Pintar <pintarj.zero@gmail.com>
2021-07-11 10:41:18 +0200

    changed the way how virtual states are identified (initial stateaggregate key)

--- src/regex/non-deterministic-states-map.ts
+++ src/regex/non-deterministic-states-map.ts
@@ -1,15 +1,41 @@
 import {Context} from './context';
 import {State} from './state';
+import {InternalGenerzError} from '@dist/error';
 
+/**
+ * Each state that is created by this class (and was not initially specified as initial-state)
+ * it's named _virtual_.
+ */
 export class NonDeterministicStatesMap {
+    /**
+     * Contains the ids of all the initial provided states.
+     */
+    private initial_states_ids: Set<number>;
+
+    /**
+     * Maps each virtual state in the _initial states_ that would represent.
+     */
+    private virtual_to_initial_states_map: Map<number, number[]>;
+
+    /**
+     * Maps the _initial states aggregation key_ to a state representing being in the states
+     * present in the key. For example: key `3,4,7` would map to a state that represent being
+     * in the states `3`, `4` and `7` contemporaneously.
+     */
     private map: Map<string, State>;
 
-    public constructor(public readonly context: Context, initial_states?: State[]) {
+    /**
+     * @param context The state machine context.
+     * @param initial_states The initial states for this class.
+     */
+    public constructor(public readonly context: Context, initial_states: State[]) {
+        this.initial_states_ids = new Set();
+        this.virtual_to_initial_states_map = new Map();
         this.map = new Map();
-
-        if (initial_states !== undefined) {
-            for (let state of initial_states)
-                this.map.set(state.id.toString(), state);
+        
+        for (let state of initial_states) {
+            this.initial_states_ids.add(state.id);
+            this.map.set(state.id.toString(), state);
         }
     }
 
@@ -21,11 +47,32 @@ export class NonDeterministicStatesMap {
      * If any of the specified states is final, then also the created non-deterministic
      * state will be final. In this case the `machine_id` of the created state will be
      * the minor id between the ones from specified final states.
+     * 
+     * @param non_deterministic_states The states to map in the new state (or existing one if
+     *     already present).
      */
     public get_or_create(non_deterministic_states: State[]): State {
-        const key   = [... new Set(non_deterministic_states.map(state => state.id))].sort().join(',');
-        let   value = this.map.get(key);
+        const states_ids = new Set<number>();
 
+        for (const state of non_deterministic_states) {
+            if (this.initial_states_ids.has(state.id)) { // if it's initial...
+                states_ids.add(state.id);
+                continue;
+            }
+
+            const sub_initial_states = this.virtual_to_initial_states_map.get(state.id);
+
+            if (sub_initial_states !== undefined) { // or it's virtual...
+                sub_initial_states.forEach(id => states_ids.add(id));
+                continue;
+            }
+            
+            InternalGenerzError.throw(`One of provided states (${state.id}) is not initial and even not virtual.`);
+        }
+        
+        const key = [... states_ids].sort((a, b) => a - b).join(',');
+        let value = this.map.get(key);
+        
         if (value !== undefined)
             return value;
 
@@ -47,6 +94,7 @@ export class NonDeterministicStatesMap {
             value.machine_id = Math.min(...machine_ids);
         }
 
+        this.virtual_to_initial_states_map.set(value.id, [...states_ids]);
         this.map.set(key, value);
         return value;
     }
--- tests/regex/non-deterministic-states-map.test.ts
+++ tests/regex/non-deterministic-states-map.test.ts
@@ -4,8 +4,9 @@ import { State } from '@dist/regex/state';
 import { SingleSymbol } from '@dist/regex/single-symbol';
 
 test('single-state-key', () => {
-    const state_0 = new State(0);
-    const map = new NonDeterministicStatesMap(new Context());
+    const context = new Context();
+    const state_0 = context.create_new_state();
+    const map = new NonDeterministicStatesMap(context, [state_0]);
     const state_0_0 = map.get_or_create([state_0]);
     const state_0_1 = map.get_or_create([state_0]);
     expect(state_0_0.id).toBe(0);
@@ -13,7 +14,8 @@ test('single-state-key', () => {
 });
 
 test('no-state-key', () => {
-    const map = new NonDeterministicStatesMap(new Context());
+    const context = new Context();
+    const map = new NonDeterministicStatesMap(context, []);
     const state_0_0 = map.get_or_create([]);
     const state_0_1 = map.get_or_create([]);
     expect(state_0_0.id).toBe(0);
@@ -21,9 +23,10 @@ test('no-state-key', () => {
 });
 
 test('single-state-keys', () => {
+    const context = new Context();
     const state_0 = new State(0);
     const state_1 = new State(1);
-    const map = new NonDeterministicStatesMap(new Context());
+    const map = new NonDeterministicStatesMap(context, [state_0, state_1]);
     const state_0_0 = map.get_or_create([state_0]);
     const state_0_1 = map.get_or_create([state_0]);
     const state_1_0 = map.get_or_create([state_1]);
@@ -35,9 +38,10 @@ test('single-state-keys', () => {
 });
 
 test('multi-state-key', () => {
+    const context = new Context();
     const state_0 = new State(0);
     const state_1 = new State(1);
-    const map = new NonDeterministicStatesMap(new Context());
+    const map = new NonDeterministicStatesMap(context, [state_0, state_1]);
     const state_0_0 = map.get_or_create([state_0, state_1]);
     const state_0_1 = map.get_or_create([state_0, state_1]);
     expect(state_0_0.id).toBe(0);
@@ -45,11 +49,12 @@ test('multi-state-key', () => {
 });
 
 test('multi-state-keys', () => {
+    const context = new Context();
     const state_0 = new State(0);
     const state_1 = new State(1);
     const state_2 = new State(2);
     const state_3 = new State(3);
-    const map = new NonDeterministicStatesMap(new Context());
+    const map = new NonDeterministicStatesMap(context, [state_0, state_1, state_2, state_3]);
     const state_0_0 = map.get_or_create([state_0, state_1]);
     const state_0_1 = map.get_or_create([state_0, state_1]);
     const state_1_0 = map.get_or_create([state_2, state_3]);
@@ -61,9 +66,10 @@ test('multi-state-keys', () => {
 });
 
 test('multi-state-key-sort', () => {
+    const context = new Context();
     const state_0 = new State(0);
     const state_1 = new State(1);
-    const map = new NonDeterministicStatesMap(new Context());
+    const map = new NonDeterministicStatesMap(context, [state_0, state_1]);
     const state_0_0 = map.get_or_create([state_0, state_1]);
     const state_0_1 = map.get_or_create([state_1, state_0]);
     expect(state_0_0.id).toBe(0);
@@ -71,8 +77,9 @@ test('multi-state-key-sort', () => {
 });
 
 test('state-repeated-in-key', () => {
+    const context = new Context();
     const state_0 = new State(0);
-    const map = new NonDeterministicStatesMap(new Context());
+    const map = new NonDeterministicStatesMap(context, [state_0]);
     const state_0_0 = map.get_or_create([state_0]);
     const state_0_1 = map.get_or_create([state_0, state_0]);
     expect(state_0_0.id).toBe(0);
@@ -80,13 +87,14 @@ test('state-repeated-in-key', () => {
 });
 
 test('initial-states', () => {
+    const context = new Context();
     const states = [
         new State(1),
         new State(2),
         new State(3),
         new State(4)
     ];
-    const map = new NonDeterministicStatesMap(new Context(), states);
+    const map = new NonDeterministicStatesMap(context, states);
     const state_0 = map.get_or_create([states[0]]);
     const state_1 = map.get_or_create([states[1]]);
     const state_2 = map.get_or_create([states[2]]);
@@ -112,45 +120,78 @@ test('initial-states-create-additional', () => {
     expect(state_0_1.id).toBe(2);
 });
 
-test('new-state-transitions-union', () => {
-    const context = new Context();
-    const states = [
-        context.create_new_state(),
-        context.create_new_state(),
-        context.create_new_state()
-    ];
-    const map = new NonDeterministicStatesMap(context, states);
-    const state_0 = map.get_or_create([states[0]]);
-    const state_1 = map.get_or_create([states[1]]);
-    const state_2 = map.get_or_create([states[2]]);
-    state_0.add_transition(new SingleSymbol(8), state_2);
-    state_1.add_transition(new SingleSymbol(16), state_2);
-    const state_0_1 = map.get_or_create([states[0], states[1]]);
-    expect(state_0.id).toBe(0);
-    expect(state_1.id).toBe(1);
-    expect(state_2.id).toBe(2);
-    expect(state_0_1.id).toBe(3);
-    expect(state_0_1.transitions.length).toBe(2);
-    expect(state_0_1.transitions[0].symbol?.contains_only(8)).toBe(true);
-    expect(state_0_1.transitions[1].symbol?.contains_only(16)).toBe(true);
-    expect(state_0_1.transitions[0].state.id).toBe(2);
-    expect(state_0_1.transitions[1].state.id).toBe(2);
+describe('overlapping', () => {
+    test('different-states-same-virtual-state', () => {
+        const context = new Context();
+        const states = [
+            context.create_new_state(),
+            context.create_new_state(),
+            context.create_new_state()
+        ];
+        const map = new NonDeterministicStatesMap(context, states);
+        const state_3 = map.get_or_create([states[0], states[2]]);
+        const state_4 = map.get_or_create([states[0], states[1], states[2]]);
+        expect(state_3.id).toEqual(3);
+        expect(state_4.id).toEqual(4);
+        expect(map.get_or_create([state_3, states[1]]).id).toEqual(4);
+    });
+    
+    test('sub-set', () => {
+        const context = new Context();
+        const states = [
+            context.create_new_state(),
+            context.create_new_state()
+        ];
+        const map = new NonDeterministicStatesMap(context, states);
+        const state_2 = map.get_or_create([states[0], states[1]]);
+        expect(state_2.id).toEqual(2);
+        expect(map.get_or_create([state_2, states[0]]).id).toEqual(2);
+        expect(map.get_or_create([state_2, states[1]]).id).toEqual(2);
+        expect(map.get_or_create([state_2, states[0], states[1]]).id).toEqual(2);
+    });
 });
 
-test('new-state-transitions-final', () => {
-    const context = new Context();
-    const states = [
-        context.create_new_state(),
-        context.create_new_state(),
-        context.create_new_state(),
-        context.create_new_state()
-    ];
-    states[2].is_final = true;
-    states[3].is_final = true;
-    const map = new NonDeterministicStatesMap(context, states);
-    expect(map.get_or_create([states[0], states[1]]).is_final).toBe(false);
-    expect(map.get_or_create([states[1], states[2]]).is_final).toBe(true);
-    expect(map.get_or_create([states[2], states[3]]).is_final).toBe(true);
+describe('transitions', () => {
+    test('union', () => {
+        const context = new Context();
+        const states = [
+            context.create_new_state(),
+            context.create_new_state(),
+            context.create_new_state()
+        ];
+        const map = new NonDeterministicStatesMap(context, states);
+        const state_0 = map.get_or_create([states[0]]);
+        const state_1 = map.get_or_create([states[1]]);
+        const state_2 = map.get_or_create([states[2]]);
+        state_0.add_transition(new SingleSymbol(8), state_2);
+        state_1.add_transition(new SingleSymbol(16), state_2);
+        const state_0_1 = map.get_or_create([states[0], states[1]]);
+        expect(state_0.id).toBe(0);
+        expect(state_1.id).toBe(1);
+        expect(state_2.id).toBe(2);
+        expect(state_0_1.id).toBe(3);
+        expect(state_0_1.transitions.length).toBe(2);
+        expect(state_0_1.transitions[0].symbol?.contains_only(8)).toBe(true);
+        expect(state_0_1.transitions[1].symbol?.contains_only(16)).toBe(true);
+        expect(state_0_1.transitions[0].state.id).toBe(2);
+        expect(state_0_1.transitions[1].state.id).toBe(2);
+    });
+    
+    test('final', () => {
+        const context = new Context();
+        const states = [
+            context.create_new_state(),
+            context.create_new_state(),
+            context.create_new_state(),
+            context.create_new_state()
+        ];
+        states[2].is_final = true;
+        states[3].is_final = true;
+        const map = new NonDeterministicStatesMap(context, states);
+        expect(map.get_or_create([states[0], states[1]]).is_final).toBe(false);
+        expect(map.get_or_create([states[1], states[2]]).is_final).toBe(true);
+        expect(map.get_or_create([states[2], states[3]]).is_final).toBe(true);
+    });
 });
 
 describe('machine-id', () => {
