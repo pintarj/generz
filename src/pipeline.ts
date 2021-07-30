89af684b62cc4cbde38d98619ff7803a30a3842f
Janez Pintar <pintarj.zero@gmail.com>
2021-07-30 14:14:49 +0200

    cli pipeline now performs stages until semantic analysis (#19)

--- src/pipeline.ts
+++ src/pipeline.ts
@@ -1,7 +1,10 @@
-import * as fs from 'fs-extra';
-import Args from './args';
-import { SourceReader } from './source/source-reader';
-import { StringReader } from './reader';
+import * as fs from 'fs-extra'
+import Args from './args'
+import { SourceReader } from './source/source-reader'
+import { StringReader } from './reader'
+import { parse as lexical_analysis } from './lexical-analysis'
+import { parse as syntax_analysis } from './syntax-analysis'
+import { analyze as semantic_analysis } from './semantic-analysis'
 
 export default class Pipeline {
     public constructor(readonly args: Args) {
@@ -9,9 +12,11 @@ export default class Pipeline {
     }
 
     public run() {
-        const source = fs.readFileSync(this.args.file, 'utf8');
-        new SourceReader(new StringReader(source), {file: this.args.file});
-        // TODO: continue with lexical analysis...
+        const source = fs.readFileSync(this.args.file, 'utf8')
+        const reader = new SourceReader(new StringReader(source), {file: this.args.file})
+        const symbols = lexical_analysis(reader)
+        const ast = syntax_analysis(this.args.file, symbols)
+        semantic_analysis(this.args.file, ast)
     }
 }
 
