d0b504c625d6661acd5a425ed71c943a354f5ced
Janez Pintar <pintarj.zero@gmail.com>
2022-08-18 19:14:55 +0200

    output file now must be specified in the CLI args

--- src/args.ts
+++ src/args.ts
@@ -3,10 +3,13 @@ import yargs from 'yargs'
 import { ArgsError } from './error'
 
 export default class Args {
-    readonly file: string
+    public readonly file: string
+    
+    public readonly output_path: string
 
     private constructor(args: any) {
         this.file = args.file
+        this.output_path = args.o
 
         if (!fs.existsSync(this.file))
             throw new ArgsError(`source file \`${this.file}\` doesn't exists`)
@@ -14,7 +17,14 @@ export default class Args {
 
     public static parse() {
         const args = yargs
+            .scriptName('generz')
             .command('$0 <file>', 'compiles an .erz source file')
+            .option('o', {
+                alias: 'output',
+                demandOption: true,
+                describe: 'the output file',
+                type: 'string'
+            })
             .parse()
 
         return new Args(args)
