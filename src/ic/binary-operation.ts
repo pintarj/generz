99fba02023d63bef416cc09b3890f4de16f9ea07
Janez Pintar <pintarj.zero@gmail.com>
2022-12-03 12:12:23 +0100

    IC Binary Operation now has the GTE and LTE operator

--- src/ic/binary-operation.ts
+++ src/ic/binary-operation.ts
@@ -6,7 +6,9 @@ export enum Operator {
     EQUAL,
     NOT_EQUAL,
     LESS_THAN,
+    LESS_THAN_OR_EQUAL,
     GREATER_THAN,
+    GREATER_THAN_OR_EQUAL,
 }
 
 export class BinaryOperation extends Expression {
