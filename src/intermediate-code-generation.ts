b67c2cdba3bced5194dec01deb5e4b925feb0baf
Janez Pintar <pintarj.zero@gmail.com>
2023-12-04 23:52:52 +0100

    intermediate code generation implemented (AST -> IC)

--- src/intermediate-code-generation.ts
+++ src/intermediate-code-generation.ts
@@ -1,17 +1,245 @@
+import { DeclarationType } from './ast/declaration'
+import { Delimiter } from './ast/delimiter'
 import { Source } from './ast/source'
+import { Terminal } from './ast/terminal'
+import { TerminalUsage } from './ast/terminal-usage'
+import { Variable } from './ast/variable'
+import { Atom } from './ic/atom'
+import { BinaryOperation, Operator } from './ic/binary-operation'
 import { Declaration } from './ic/declaration'
+import { DeclarationStatement } from './ic/declaration-statement'
+import { Function } from './ic/function'
+import { FunctionCall } from './ic/function-call'
+import { If } from './ic/if'
+import { Return } from './ic/return'
+import { Statement } from './ic/statement'
+import { Statements } from './ic/statements'
+import { VariableDeclaration, VariableType } from './ic/variable-declaration'
+import { regex_to_ic } from './regex-to-ic'
 import { Context } from './regex/context'
+import { RegularExpression } from './regular-expression'
 
 class ICGenerator {
+    private readonly variables_map: Map<string, Variable>
+    private readonly terminals_map: Map<string, Terminal&{id: number}>
+    private readonly terminals_parsing_functions_map: Map<string, Function>
+    private readonly delimiter: Delimiter|undefined
+
     public constructor(
-        _regex_context: Context,
-        _ast: Source
+        private regex_context: Context,
+        ast: Source
     ) {
-        
+        this.variables_map = new Map()
+        this.terminals_map = new Map()
+        this.terminals_parsing_functions_map = new Map()
+
+        for (const declaration of ast.declarations) {
+            switch (declaration.type) {
+                case DeclarationType.DELIMITER: {
+                    this.delimiter = declaration as Delimiter
+                    break
+                }
+
+                case DeclarationType.TERMINAL: {
+                    const id = this.terminals_map.size + 1
+                    const terminal = Object.assign(declaration as Terminal, {id})           
+                    terminal.regex.machine_id = id
+                    this.terminals_map.set(declaration.name, terminal)
+                    break
+                }
+
+                case DeclarationType.VARIABLE: {
+                    this.variables_map.set(declaration.name, declaration as Variable)
+                    break
+                }
+            }
+        }
     }
-    
+
+    private generate_function_for_parsing_terminals(terminals: Terminal[], options?: {epsilon?: boolean}): Function {
+        const epsilon = options?.epsilon === true
+        const terminals_names = terminals.map(x => x.name).sort() 
+        const name = `parse${epsilon ? '_eps' : ''}_${terminals_names.map(x => `T${x}`).join('_')}`
+        let f = this.terminals_parsing_functions_map.get(name)
+
+        if (f === undefined) {
+            const parsed_terminal_id_var = new VariableDeclaration(VariableType.I32, 'parsed_terminal_id', {
+                mutable: true,
+                initial_value: new Atom(-1),
+                comment: 'Contains the id of the last parsed terminal,\nor -1 if there was no terminal successfully parsed.'
+            })
+
+            const parsed_terminal_id_ref = parsed_terminal_id_var.get_reference()
+            const machine = RegularExpression.merge(this.regex_context, terminals.map(x => x.regex))
+            
+            const statements: Statement[] = [
+                parsed_terminal_id_var.to_statement(),
+                regex_to_ic(machine, {parsed_terminal_id_ref})
+            ]
+
+            if (!epsilon) {
+                const error_message = (terminals.length === 1
+                    ? `expected terminal \`${terminals[0].name}\`, `
+                    : `expected one of terminals: ${terminals.map(x => `\`${x.name}\``).join(', ')}; `
+                ) + `but found "{}"`
+
+                const statement = new If(
+                    new BinaryOperation(Operator.EQUAL, parsed_terminal_id_ref, new Atom(-1)),
+                    (new FunctionCall('throw_error', {args: [
+                        new Atom(error_message),
+                        new FunctionCall('read')
+                    ]})).to_statement()
+                )
+
+                statements.push(statement)
+            }
+
+            statements.push(new Return(parsed_terminal_id_ref))
+
+            f = new Function(name, [], VariableType.I32, new Statements(statements), {
+                comment: `Parses one of terminals: ${terminals_names.map(x => `\`${x}\``).join(', ')}.`
+            })
+
+            this.terminals_parsing_functions_map.set(name, f)
+        }
+
+        return f
+    }
+
+    private generate_function_consume_delimiter(delimiter: Delimiter): Function {
+        return new Function('consume_delimiter', [], VariableType.VOID, new Statements([
+            regex_to_ic(delimiter.regex),
+            (new FunctionCall('reset')).to_statement()    
+        ]))
+    }
+
+    private generate_function_for_parsing_variable(variable: Variable): Function {
+        const all_possible_terminals = variable.terminals_maps.map(m => m.terminals).flat()
+        const parsing_function = this.generate_function_for_parsing_terminals(all_possible_terminals, {epsilon: variable.epsilon})
+
+        const variable_terminal_id = new VariableDeclaration(VariableType.I32, 'terminal_id', {
+            initial_value: new FunctionCall(parsing_function.name)
+        })
+
+        let branches: Statement|undefined = !variable.epsilon
+            ? (new FunctionCall('throw_error', {args: [
+                new Atom('program reached invalid state: unexpected terminal (id: {}) parsed'),
+                variable_terminal_id.get_reference()
+            ]})).to_statement()
+            : undefined
+
+        for (const terminals_map of variable.terminals_maps) {
+            const statements: Statement[] = []
+
+            for (const node of terminals_map.nodes.slice(1)) {
+                if (node.name.charAt(0) == node.name.charAt(0).toLocaleLowerCase()) {
+                    const terminal_parsing_function = this.generate_function_for_parsing_terminals([(node as TerminalUsage).reference])
+
+                    if (this.delimiter !== undefined) {
+                        statements.push((new FunctionCall('consume_delimiter')).to_statement())
+                    }
+
+                    statements.push(
+                        (new FunctionCall(terminal_parsing_function.name)).to_statement(),
+                        (new FunctionCall('reset')).to_statement()
+                    )
+                } else {
+                    statements.push(
+                        (new FunctionCall(`parse_V${node.name}`)).to_statement()
+                    )
+                }
+            }
+
+            const condition = terminals_map.terminals.map(x =>
+                new BinaryOperation(
+                    Operator.EQUAL,
+                    variable_terminal_id.get_reference(),
+                    new Atom(this.terminals_map.get(x.name)!.id)
+                )
+            ).reduce((left, right) =>
+                new BinaryOperation(
+                    Operator.OR,
+                    left,
+                    right
+                )
+            )
+
+            branches = new If(
+                condition,
+                new Statements(statements),
+                {
+                    else_body: branches
+                }
+            )
+        }
+
+        if (branches === undefined)
+            throw new Error('creating variable parsing function without branches')
+
+        const body = new Statements([])
+
+        if (this.delimiter !== undefined) {
+            body.statements.push((new FunctionCall('consume_delimiter')).to_statement())
+        }
+
+        body.statements.push(
+            new DeclarationStatement(variable_terminal_id, {
+                comment: 'The id of the currently parsed terminal.'
+            }),
+            (new FunctionCall('reset')).to_statement(),
+            branches
+        )
+
+        return new Function(`parse_V${variable.name}`, [], VariableType.VOID, body, {
+            comment: `Function that parses the \`${variable.name}\` variable.`
+        })
+    }
+
+    public generate_function_root(root_variable_parsing_function: Function): Function {
+        const var_last_char = new VariableDeclaration(VariableType.I32, 'last_char', {
+            initial_value: new FunctionCall('next')
+        })
+
+        return new Function('root', [], VariableType.VOID, new Statements([
+            (new FunctionCall(root_variable_parsing_function.name)).to_statement(),
+            (new FunctionCall('reset')).to_statement(),
+            ...((() => {
+                return this.delimiter !== undefined
+                    ? [(new FunctionCall('consume_delimiter')).to_statement()]
+                    : []
+            }) ()),
+            var_last_char.to_statement(),
+            new If(
+                new BinaryOperation(
+                    Operator.NOT_EQUAL,
+                    var_last_char.get_reference(),
+                    new Atom(0)
+                ),
+                (new FunctionCall('throw_error', {
+                    args: [
+                        new Atom('EOF expected, but something else found')
+                    ]
+                })).to_statement()
+            )
+        ]))
+    }
+
     public generate(): Declaration[] {
-        return []
+        const declarations: Declaration[] = []
+
+        if (this.delimiter !== undefined) {
+            declarations.push(this.generate_function_consume_delimiter(this.delimiter))
+        }
+
+        const variables_parsing_functions = Array.from(this.variables_map.values()).map(x => this.generate_function_for_parsing_variable(x))
+        
+        declarations.push(
+            ...this.terminals_parsing_functions_map.values(),
+            ...variables_parsing_functions,
+            this.generate_function_root(variables_parsing_functions[variables_parsing_functions.length - 1])
+        )
+
+        return declarations
     }
 }
 
---
+++ tests/intermediate-code-generation.test.ts
@@ -0,0 +1,220 @@
+import { parse as lexical_analysis } from '@dist/lexical-analysis'
+import { parse as syntax_analysis } from '@dist/syntax-analysis'
+import { analyze as semantic_analysis } from '@dist/semantic-analysis'
+import { SourceReader } from '@dist/source/source-reader'
+import { StringReader } from '@dist/reader'
+import { Context } from '@dist/regex/context'
+import { generate as ic_generate} from '@dist/intermediate-code-generation'
+import { Declaration } from '@dist/ic/declaration'
+import dedent from 'dedent'
+import { IcExecutionMachine } from '@dist/ic-execution-machine'
+import { FunctionCall } from '@dist/ic/function-call'
+
+class ParserState {
+    private readonly iterator: Iterator<number>
+    private buffer: number[]
+    private cursor_index: number
+    private marked_length: number
+
+    public constructor(source: Iterable<number>) {
+        this.iterator = source[Symbol.iterator]()
+        this.buffer = []
+        this.cursor_index = -1
+        this.marked_length = -1
+    }
+
+    public current(): number {
+        if (this.cursor_index === -1)
+            throw new Error('trying to read from uninitialized parser')
+
+        if (this.buffer.length === this.cursor_index) {
+            const result = this.iterator.next()
+            this.buffer.push(result.done ? 0 : result.value)
+        }
+
+        return this.buffer[this.cursor_index]
+    }
+
+    public mark(offset?: number): void {
+        this.marked_length = this.cursor_index + 1 + (offset || 0)
+    }
+
+    public next(): number {
+        this.advance()
+        return this.current()
+    }
+
+    public advance(): void {
+        this.cursor_index += 1
+    }
+
+    public reset(): void {
+        if (this.marked_length !== -1)
+            this.buffer = this.buffer.slice(this.marked_length)
+
+        this.cursor_index = -1
+        this.marked_length = -1
+    }
+
+    public read(): string {
+        return this.buffer.slice(0, this.cursor_index + 1).map(x => String.fromCharCode(x)).join('')
+    }
+}
+
+class VirtualParser {
+    private readonly ic_machine: IcExecutionMachine
+    private state: ParserState|undefined
+
+    public constructor(declarations: Declaration[]) {
+        this.state = undefined
+        this.ic_machine = new IcExecutionMachine()
+
+        for (const declaration of declarations) {
+            this.ic_machine.execute(declaration.to_statement())
+        }
+    
+        this.ic_machine.global_scope.declare_function('current', () => this.state?.current())
+        this.ic_machine.global_scope.declare_function('mark', offset => this.state?.mark(offset))
+        this.ic_machine.global_scope.declare_function('next', () => this.state?.next())
+        this.ic_machine.global_scope.declare_function('advance', () => this.state?.advance())
+        this.ic_machine.global_scope.declare_function('reset', () => this.state?.reset())
+        this.ic_machine.global_scope.declare_function('read', () => this.state?.read())
+
+        this.ic_machine.global_scope.declare_function('throw_error', (...args) => {
+            let index = 1
+            throw new Error((args[0] as string).replaceAll('{}', () => args[index++]))
+        })
+    }
+
+    public parse(input: string): void {
+        this.state = new ParserState(Array.from(input).map(x => x.charCodeAt(0)))
+        this.ic_machine.execute((new FunctionCall('root')).to_statement())
+        this.state = undefined
+    }
+}
+
+function f(source: string): Declaration[] {
+    const file = 'fake.erz'
+    const context = new Context()
+    const ast = syntax_analysis(context, file, lexical_analysis(new SourceReader(new StringReader(source), {file})))
+    semantic_analysis(file, ast)
+    return ic_generate(context, ast)
+}
+
+describe('a', () => {
+    let parser: VirtualParser|undefined
+
+    beforeAll(() => {
+        parser = new VirtualParser(f(dedent`
+            terminal a /a/
+    
+            variable A {
+                production a
+            }
+        `))
+    })
+
+    test('empty', async () => {
+        expect(() => parser!.parse('')).toThrowError(/expected.+terminal.+but found/)
+    })
+
+    test('a', async () => {
+        expect(() => parser!.parse('a')).not.toThrow()
+    })
+
+    test('wrong', async () => {
+        expect(() => parser!.parse('1111')).toThrowError(/expected.+terminal.+but found/)
+    })
+})
+
+describe('binary', () => {
+    let parser: VirtualParser|undefined
+
+    beforeAll(() => {
+        parser = new VirtualParser(f(dedent`
+            delimiter /\s+/
+            terminal zero /0/
+            terminal one /1/
+    
+            variable XExt {
+                production zero XExt
+                production one XExt
+                epsilon
+            }
+    
+            variable X {
+                production zero XExt
+                production one XExt
+            }
+        `))
+    })
+
+    test('empty', async () => {
+        expect(() => parser!.parse('')).toThrowError(/expected.+terminal.+but found/)
+    })
+
+    test('just-zeros', async () => {
+        expect(() => parser!.parse('0000')).not.toThrow()
+    })
+
+    test('just-ones', async () => {
+        expect(() => parser!.parse('1111')).not.toThrow()
+    })
+
+    test('mixed', async () => {
+        expect(() => parser!.parse('0010101110101')).not.toThrow()
+    })
+
+    test('whitespaces', async () => {
+        expect(() => parser!.parse('00101\n01\r110\t10 1')).not.toThrow()
+    })
+
+    test('wrong', async () => {
+        expect(() => parser!.parse('2')).toThrowError(/expected.+terminal.+but found/)
+    })
+})
+
+describe('numexp', () => {
+    let parser: VirtualParser|undefined
+
+    beforeAll(() => {
+        parser = new VirtualParser(f(dedent`
+            delimiter /\s+/
+            terminal number /[0-9]+/
+            terminal plus /\+/
+            terminal minus /\-/
+
+            variable XExt {
+                production plus number XExt
+                production minus number XExt
+                epsilon
+            }
+
+            variable X {
+                production number XExt
+            }
+        `))
+    })
+
+    const good_ones = [
+        '4 + 2',
+        '1-2',
+        '11',
+        '123 + 19 - 1',
+        ' 1+2'
+    ]
+
+    for (const query of good_ones) {
+        test(query.replaceAll(' ', ''), async () => {
+            expect(() => parser!.parse(query)).not.toThrow()
+        })
+    }
+
+    test('5+-1', async () => {
+        expect(() => parser!.parse('5 + -1')).toThrowError(/expected.+terminal.+number.+but found.+\-/)
+    })
+
+    test('5+xyz', async () => {
+        expect(() => parser!.parse('5 + xyz')).toThrowError(/expected.+terminal.+number.+but found.+x/)
+    })
+})
